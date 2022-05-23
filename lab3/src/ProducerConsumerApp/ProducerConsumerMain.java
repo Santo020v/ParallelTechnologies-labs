c6e4c7c20492dcd9158b0eff0cc2297d3c49fe75
Veronika <54397786+Santo020v@users.noreply.github.com>
2022-05-23 18:03:07 +0300

    Delete lab3 directory

--- lab3/src/ProducerConsumerApp/ProducerConsumerMain.java
+++
@@ -1,9 +0,0 @@
-package ProducerConsumerApp;
-
-public class ProducerConsumerMain {
-    public static void main(String[] args) {
-        Drop drop = new Drop();
-        (new Thread(new Producer(drop))).start();
-        (new Thread(new Consumer(drop))).start();
-    }
-}
